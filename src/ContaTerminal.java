2afd4406a93a7af56c153cf62f65f767478af031
Thomas <thomasprof21@gmail.com>
2023-09-14 22:19:23 -0300

    primeiro commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO:Conhecer e importar a classe Scanner
+        Scanner teclado = new Scanner(System.in);
+        Integer numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+           boolean validacao = false;
+        //Exibir as mensagens para o nosso usuário
+
+        System.out.println("Olá seja bem-vindo !");
+        System.out.println("Digite o Seu nome: ");
+        nomeCliente = teclado.nextLine();
+        System.out.println("--------------------");
+     
+        System.out.println("Digite a sua Agência: ");
+        agencia = teclado.nextLine().replaceAll("[^\\d]", "").replaceAll("...", "$0-");
+    
+        System.out.println("--------------------");
+        System.out.println("Digite o Numero: ");
+        numero = teclado.nextInt();
+        System.out.println("--------------------");
+        System.out.println("Digite o seu saldo: ");
+        saldo = teclado.nextDouble();
+        teclado.close();
+        //Exibir a mensagem conta criada 
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d e seu saldo é %.2f",nomeCliente,agencia,numero,saldo);
+    }
+}
+
